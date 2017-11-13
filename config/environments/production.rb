1480ca80a5eb6488960d4d25ff4dfee4a639fade
Michael Ramassamy <sunsmiley@live.fr>
2017-11-14 00:37:39 +0100

    change mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method     = :sparkpost
-  config.action_mailer.default_url_options = { host: "langeconnect.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "langeconnect.fr" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
