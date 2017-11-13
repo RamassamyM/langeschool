60819985146196702b942e93d13911ee9d705204
Michael Ramassamy <sunsmiley@live.fr>
2017-11-13 17:48:49 +0100

    Update smtp.rb

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.sparkpostmail.com",
   port: 587,
   domain: 'gmail.com',
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_APP_PASSWORD'],
+  user_name: ENV['SPARKPOST_USERNAME'],
+  password: ENV['SPARKPOST_PASSWORD'],
   authentication: :login,
   enable_starttls_auto: true
 }
